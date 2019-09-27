43a8da39cc2addfc211fee66f35e60d26ecd9106
loading <604237073@qq.com>
2019-09-27 11:41:12 +0800

    熟悉webpack原理;手写自己的webpack

---
+++ bin/msl-webpack.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+console.log('start')
+
+const path = require('path')
+
+let config = require(path.resolve('webpack.config.js'))
+
+let Compiler = require('../lib/Compiler.js')
+let compiler = new Compiler(config)
+
+compiler.run();
