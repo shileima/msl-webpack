057d70c664077a22aeefe43344885935ecd05008
loading <604237073@qq.com>
2019-09-27 14:47:09 +0800

    增加支持webpack插件功能

--- bin/msl-webpack.js
+++ bin/msl-webpack.js
@@ -8,5 +8,5 @@ let config = require(path.resolve('webpack.config.js'))
 
 let Compiler = require('../lib/Compiler.js')
 let compiler = new Compiler(config)
-
+//compiler.hooks.entryOption.call();
 compiler.run();
--- webpack-dev/webpack.config.js
+++ webpack-dev/webpack.config.js
@@ -1,5 +1,20 @@
 const path = require('path')
-
+class P {
+    apply(compiler) {
+        console.log('mock P plugin')
+        compiler.hooks.emit.tap('emit', function () {
+            console.log('emit')
+        })
+    }
+}
+class P1 {
+    apply(compiler) {
+        console.log('mock P1 plugin')
+        compiler.hooks.afterPlugins.tap('emit', function () {
+            console.log('afterPlugins')
+        })
+    }
+}
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -9,13 +24,17 @@ module.exports = {
     },
     module: {
         rules: [
-            { 
+            {
                 test: /\.less$/,
-                use:[
-                    path.resolve(__dirname,'loader','style-loader'),
-                    path.resolve(__dirname,'loader','less-loader')
+                use: [
+                    path.resolve(__dirname, 'loader', 'style-loader'),
+                    path.resolve(__dirname, 'loader', 'less-loader')
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new P(),
+        new P1()
+    ]
 }
